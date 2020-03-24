e85d1372d754370c80749bcc2dd0578226befecb
Andrew Merrill <realcoldfate@gmail.com>
2020-03-24 00:30:26 -0400

    Added JavaDoc for LoadCommand and InputHandler

--- InputHandler.java
+++ InputHandler.java
@@ -1,10 +1,23 @@
-
 import java.util.HashMap;
 
+/**
+ *  A InputHandler Object
+ *	A class to take in string input and execute the corresponding command
+ *
+ * @author Andrew Merrill, Jake Brown
+ * @Version 1.0.0 March 24, 2020
+ */
 public class InputHandler {
 
+	/**
+	 * A HashMap to store the command string and command object pairing
+	 */
 	private HashMap<String, Command> commands = new HashMap<String, Command>();
 
+
+	/**
+	 * @param document the document the input will be accessing
+	 */
 	public InputHandler(Document document) {
 		commands.put("load", new LoadCommand(document));
 		commands.put("save", new SaveCommand(document));
@@ -12,8 +25,12 @@ public class InputHandler {
 		commands.put("print", new PrintCommand(document));
 	}
 
+	/**
+	 * Takes in a String to be run as a command
+	 * @param data this string that will act as the key to run the command
+	 */
 	public void inputEntered(String data) {
-		if(commands.get(data) != null)
+		if(commands.containsKey(data))
 			commands.get(data).execute();
 		else
 			System.out.println("Sorry, we don't recognize that command");
--- LoadCommand.java
+++ LoadCommand.java
@@ -1,13 +1,26 @@
-// Load Command Goes here
-
+/**
+ * A class that acts as a command to load a Document
+ *
+ * @author Andrew Merrill
+ * @version 1.0.0 March 24, 2020
+ */
 public class LoadCommand implements Command {
 
+	/**
+	 * The Document the command will be running on
+	 */
 	private Document doc;
 
+	/**
+	 * @param doc input to be stored as the Document to be acted on
+	 */
 	public LoadCommand(Document doc) {
 		this.doc = doc;
 	}
 
+	/**
+	 * Executes the load command
+	 */
 	public void execute() {
 		doc.load();
 	}
